58f5921164a12213367a5cc18aee95f093e09151
wilsjo2 <97138003+wilsjo2@users.noreply.github.com>
2026-09-12 21:23:52 +1000

    Revert "Isolate game Streamline plugins from active DLSSG output"
    This reverts commit ae9a50fa12b1e81600d0394108d523dad8b7fd23.

--- OptiScaler/hooks/Streamline_Hooks.h
+++ OptiScaler/hooks/Streamline_Hooks.h
@@ -143,7 +143,6 @@ class StreamlineHooks
     static void applyMenuDlssgInterlock(sl::DLSSGOptions& options, bool potentiallyActive);
 
     static void unhookInterposer();
-    static HMODULE LoadIsolatedGamePlugin(LPCWSTR requestedPath);
     static void hookInterposer(HMODULE slInterposer);
 
     static void unhookDlss();
